80a84d07137b90890c494c9502d438ab9465c7ea
Der Hobbyelektroniker <der.hobbyelektroniker@gmail.com>
2020-12-07 19:02:23 +0100

    First release

---
+++ 001_Einleitung und erste Schritte/hallo.py
@@ -0,0 +1,15 @@
+# Hallo!
+
+namen = ("Max","Peter","Monika","Petra")
+
+def sage_hallo(name):
+    print("Hallo " + name)
+    
+for name in namen:
+    sage_hallo(name)
+
+print("Das war's!")
+
+
+    
+    
